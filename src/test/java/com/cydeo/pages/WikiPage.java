56c07322ee16f40b31f4b1b7a045e75e937b5512
valeriabursuk <valeria.bursuk@gmail.com>
2022-11-07 11:42:47 -0500

    no base

--- src/test/java/com/cydeo/pages/WikiPage.java
+++ src/test/java/com/cydeo/pages/WikiPage.java
@@ -1,9 +1,14 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class WikiPage extends BasePage {
+public class WikiPage {
+    public WikiPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
     @FindBy(id = "searchInput")
     private WebElement searchbox;
