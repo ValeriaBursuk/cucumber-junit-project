8a6bfbab3f9aa9add5720d47fd34c82c0b6c6c4f
valeriabursuk <valeria.bursuk@gmail.com>
2022-11-10 15:47:23 -0500

    driver with grid

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -14,10 +14,10 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features" , //FEATURES PATH copy CONTENT ROOT
         glue = "com/cydeo/step_definitions",//glue - STEP DEFINITIONS PATH copy SOURCE ROOT --> runs
-        publish = true
+        publish = true,
         // hooks first
         //dryRun = true,//- returns snippets,
-       // tags = "@smoke" //if no tags - runs everything
+        tags = "@wip" //if no tags - runs everything
 )
 
 public class CukesRunner {
